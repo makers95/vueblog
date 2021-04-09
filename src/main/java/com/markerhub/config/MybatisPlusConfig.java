5d6099cdb5472dfe01ee56c77cfe48de6757391c
stream <stream2jazz@gmail.com>
2021-04-09 14:03:59 +0800

    Initial commit

---
+++ src/main/java/com/markerhub/config/MybatisPlusConfig.java
@@ -0,0 +1,24 @@
+package com.markerhub.config;
+
+import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+/**
+ * @Description
+ * @Param $
+ * @return $
+ **/
+@Configuration
+@EnableTransactionManagement
+@MapperScan("com.markerhub.mapper")
+public class MybatisPlusConfig {
+    @Bean
+    public PaginationInterceptor paginationInterceptor() {
+        PaginationInterceptor paginationInterceptor = new PaginationInterceptor();
+        return paginationInterceptor;
+    }
+}
+
